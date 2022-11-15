5341e906bedbb09becce400b0644b00a90863c85
Lucas Ximenes <lucasdximenes@gmail.com>
2022-11-15 14:44:31 -0300

    test: :test_tube: TDD
    Add middlewares tests

---
+++ tests/unit/middlewares/products.middlewares.test.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+const { productsMiddlewares } = require('../../../src/middlewares');
+
+describe('Test validateId middeware', function () {
+  it('should return an error if id is string', async function () {
+    const res = {};
+    const req = { params: { id: 'test' } };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    await productsMiddlewares.validateId(req, res);
+
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({
+      message: '"value" must be a number',
+    });
+  });
+
+  it('should return an error if id is negative', async function () {
+    const res = {};
+    const req = { params: { id: -1 } };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    await productsMiddlewares.validateId(req, res);
+
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({
+      message: '"value" must be greater than or equal to 1',
+    });
+  });
+
+  it('should call next if id is number', async function () {
+    const res = {};
+    const req = { params: { id: 1 } };
+    const next = sinon.stub().returns();
+
+    await productsMiddlewares.validateId(req, res, next);
+
+    expect(next).to.have.been.called;
+  });
+});
