124a1b034688d684d7f5edfb04dd1a46fd8fa8bd
Lucas Ximenes <lucasdximenes@gmail.com>
2022-11-17 19:59:16 -0300

    docs: :memo: Add API Documentation files
    Change some projects archives to serve api index.html, adds dockerfile entrypoints, adds .env.example and remove unused packages

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -7,12 +8,8 @@ const routes = require('./routes');
 app.use(express.json());
 app.use(routes);
 
-// não remova esse endpoint, é para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-// não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
-
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
